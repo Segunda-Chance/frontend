4d292d78f86ff56e6fa97fcfec3bb5e3d7d3b948
NathanLPS <nathan_lopes147@hotmail.com>
2024-01-19 14:59:31 -0300

    Sobre

---
+++ src/pages/sobre/Sobre.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Sobre() {
+    return (
+        <div className = "container mx-auto p-4 text-center" >
+            <h1 className="text-3xl font-bold mb-4">Projeto Segunda Chance</h1>
+            <img
+                className="mx-auto mb-4 rounded-full"
+                src="https://ik.imagekit.io/id3pm8vmy/mendi.jpg?updatedAt=1705676331820"
+                alt="Imagem do Projeto"
+                width="500"
+                height="150"
+            />
+            <p className="text-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ab provident tenetur? Magnam molestias in corrupti quia doloremque voluptas sapiente cum, dignissimos officia doloribus facilis, corporis odit reiciendis amet quam?
+            </p>
+        </div >
+    )
+}
+
+export default Sobre
+
+
+
