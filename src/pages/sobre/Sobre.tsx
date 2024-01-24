4add9d387e0a9ecdf1226b6ff02727ced68e1350
NathanLPS <nathan_lopes147@hotmail.com>
2024-01-24 09:01:45 -0300

    Task_09

--- src/pages/sobre/Sobre.tsx
+++ src/pages/sobre/Sobre.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Sobre() {
     return (
         <div className = "container mx-auto p-4 text-center" >
-            <h1 className="text-3xl font-bold mb-4">Projeto Segunda Chance</h1>
+            <h1 className="text-3xl font-bold mb-4">Projeto Segunda Chance</h1>yarn run tailwindcss init -p
             <img
                 className="mx-auto mb-4 rounded-full"
                 src="https://ik.imagekit.io/id3pm8vmy/mendi.jpg?updatedAt=1705676331820"
