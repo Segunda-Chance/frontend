501ca1b28b70b75393b5a5fa84ec153bf9df686d
Vinicius Monteiro <moonkraft_dev@outlook.com>
2024-02-04 01:20:38 -0300

    Estilização Categoria

--- src/components/categorias/listaCategorias/ListaCategorias.tsx
+++ src/components/categorias/listaCategorias/ListaCategorias.tsx
@@ -50,9 +50,9 @@ function ListaCategorias(){
             wrapperClass="dna-wrapper mx-auto"
         />
         )}
-            <div className="flex justify-center w-full my-4">
+            <div className="flex justify-center w-full my-1">
                 <div className="container flex flex-col">
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-12">
                         <>
                             {categorias.map((categoria) => (
                                 <>
