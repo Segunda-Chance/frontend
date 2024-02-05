ef94cb9a9efd531a24f5b1e1af26e53e98f844b3
NathanLPS <nathan_lopes147@hotmail.com>
2024-02-05 16:06:12 -0300

    Toast Alert

--- src/App.tsx
+++ src/App.tsx
@@ -11,45 +11,48 @@ import Contato from './pages/contato/Contato'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Sobre from './pages/sobre/Sobre'
-
 import DeletarProduto from './components/produtos/deletarProduto/DeletarProduto'
 import FormProduto from './components/produtos/formProdutos/FormProduto'
 import ListarProdutos from './components/produtos/listarProdutos/ListarProdutos'
 import Cart from './components/produtos/cart/Cart'
 import { CartProvider } from './contexts/CartContext'
 import Perfil from './pages/perfil/Perfil'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 
 
 function App() {
   return (
     <>
-    <CartProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/contato" element={<Contato />} />
-              <Route path="/sobre" element={<Sobre />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastrarCategoria" element={<FormCategoria />} />
-              <Route path="/editarCategoria/:id" element={<FormCategoria />} />
-              <Route path="/categorias" element={<ListaCategorias />} />
-              <Route path="/deletarCategoria/:id" element={<DeletarCategorias />} />
-              <Route path="/deletarproduto/:id" element={<DeletarProduto />} />
-              <Route path="/cadastroProduto" element={<FormProduto />} />
-              <Route path="/editarproduto/:id" element={<FormProduto />} />
-              <Route path="/produtos" element={<ListarProdutos />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/perfil" element={<Perfil />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-      </AuthProvider>
+      <CartProvider>
+        <AuthProvider>
+          <ToastContainer />
+          <BrowserRouter>
+            <Navbar />
+            <div className='min-h-[80vh]'>
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/contato" element={<Contato />} />
+                <Route path="/sobre" element={<Sobre />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cadastrarCategoria" element={<FormCategoria />} />
+                <Route path="/editarCategoria/:id" element={<FormCategoria />} />
+                <Route path="/categorias" element={<ListaCategorias />} />
+                <Route path="/deletarCategoria/:id" element={<DeletarCategorias />} />
+                <Route path="/deletarproduto/:id" element={<DeletarProduto />} />
+                <Route path="/cadastroProduto" element={<FormProduto />} />
+                <Route path="/editarProduto/:id" element={<FormProduto />} />
+                <Route path="/produtos" element={<ListarProdutos />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/perfil" element={<Perfil />} />
+              </Routes>
+            </div>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
       </CartProvider>
     </>
   )
