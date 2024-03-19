44ddcf6f0bb98ff48a056ec244c2cd5b8459c497
LuizEduardoSC <luizeduardoedd1@gmail.com>
2024-03-19 02:13:03 -0300

    Teste Deploy

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,16 @@
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 import FormCatListHome from '../../components/categorias/categoriaDetalhes/FormCatListHome';
 import Cta from '../../components/cta/Cta';
 import Features from '../../components/features/Features';
 import ListarProdutos from '../../components/produtos/listarProdutos/ListarProdutos';
-import { FiltroCat } from '../../contexts/ListarCatContext';
-import './Home.css';
 import Stats from '../../components/stats/Stats';
+import './Home.css';
 
 function Home() {
   const [filtro, setFiltro] = useState('');
 
   return (
-    <FiltroCat.Provider value={[filtro, setFiltro]}>
+   /*  <FiltroCat.Provider value={[filtro, setFiltro]}> */
       <div className=" bg-white_1">
         <div>
           <Cta />
@@ -40,17 +39,17 @@ function Home() {
           <br />
           <div className="divInfo flex text-center flex-col">
             <span className="infoProduto flex text-center  justify-center m-auto items-center">
-              <img className="imginfo pr-3 " src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAQ0lEQVR4nO3RQREAIAwDwbiqK8TUMCjgydDH7swZSBIAAAAAAAAAAAAAAICZVtdWXTf4/Q8AAAAAAAAAAAAAAAB54gAa0p8x49LrUAAAAABJRU5ErkJggg=="/>
-                Produtos
-              <img className="imginfo pl-3 " src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAQ0lEQVR4nO3RQREAIAwDwbiqK8TUMCjgydDH7swZSBIAAAAAAAAAAAAAAICZVtdWXTf4/Q8AAAAAAAAAAAAAAAB54gAa0p8x49LrUAAAAABJRU5ErkJggg=="/>
+              <img className="imginfo pr-3 " src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAQ0lEQVR4nO3RQREAIAwDwbiqK8TUMCjgydDH7swZSBIAAAAAAAAAAAAAAICZVtdWXTf4/Q8AAAAAAAAAAAAAAAB54gAa0p8x49LrUAAAAABJRU5ErkJggg==" />
+              Produtos
+              <img className="imginfo pl-3 " src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQCAYAAACOEfKtAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAQ0lEQVR4nO3RQREAIAwDwbiqK8TUMCjgydDH7swZSBIAAAAAAAAAAAAAAICZVtdWXTf4/Q8AAAAAAAAAAAAAAAB54gAa0p8x49LrUAAAAABJRU5ErkJggg==" />
             </span>
           </div>
         </div>
         <ListarProdutos />
-        <Stats/>
+        <Stats />
         <Features />
       </div>
-    </FiltroCat.Provider>
+   /*  </FiltroCat.Provider> */
   );
 }
 
