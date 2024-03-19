7ec3ae571d8d43c48444dffa25fd1ddccaffc4f9
LuizEduardoSC <luizeduardoedd1@gmail.com>
2024-03-18 23:36:54 -0300

    Deploy da Versão Final do Projeto Integrador

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://segunda-chance.onrender.com/'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarProduto = async (url: string, dados: Object, setDados: Function) => {
