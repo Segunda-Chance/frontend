9803213f87455e12a84e7c2b464843319bb042c2
Vinicius Monteiro <137289894+mcs-vinicius@users.noreply.github.com>
2024-08-24 22:52:23 -0300

    Att Service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://backend-segundachance.onrender.com/'
+    baseURL: 'https://backend-segundachance.onrender.com'
 })
 
 export const cadastrarProduto = async (url: string, dados: Object, setDados: Function) => {
